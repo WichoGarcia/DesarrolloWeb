568ec2dcfbab6d8d0d29dbba540dac42380c6154
WichoGarcia <A01021865@itesm.mx>
2021-08-31 21:31:19 -0500

    Cronometro funcionando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-//librerias ó components
 import "./App.css";
 import "./index.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+
 
 function App() {
   
-  const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
+  const [date, setDate] = useState(new Date());
+  var [seconds, setSeconds] = useState(0);
+  var [minutes, setMinutes] = useState(0);
+  var [hours, setHours] = useState(0);
 
-  const onClick = () => {};  
+  
+  useEffect(() => {
+    let interval = null;
+    
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+        if(seconds===59){
+          setSeconds(seconds=>0)
+          setMinutes(minutes=>minutes+1)
+          if(minutes===59){
+            setMinutes(seconds=>0)
+            setHours(hours=>hours+1)
+          }
+        }
+      }, 1000);
+    
+    return () => clearInterval(interval);
+  });
+
+
+
+  useEffect(() => {
+    var timerID = setInterval( () => tick(), 1000 );
+  
+    return function cleanup() {
+        clearInterval(timerID);
+      };
+   });
+  
+     function tick() {
+      setDate(new Date());
+     }
+  
+     
 
   return (
     <div>
-      <input type="button" onClick={onClick} value="Update" />
-      <div>
-        {users.map((e) => (
-          <div>
-            {e.id} - {e.name}
-          </div>
-        ))}
-      </div>
+      <h1>{hours}:{minutes}:{seconds}</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
+      
     </div>
   );
 }
